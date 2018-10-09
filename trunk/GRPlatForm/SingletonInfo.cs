ac7ceb39d0fa1032f198377f7cd15dbdca9bcbfe
CavanOrBxj <40481651+CavanOrBxj@users.noreply.github.com>
2018-10-09 09:35:07 +0800

    融合平台对接程序通用版建仓

---
+++ trunk/GRPlatForm/SingletonInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Collections.Generic;
+using System.Data;
+
+namespace GRPlatForm
+{
+    public class SingletonInfo
+    {
+        private static SingletonInfo _singleton;
+
+
+        public string Longitude;//经度
+        public string Latitude;//纬度
+        public string CurrentURL;//当前平台对县平台的url
+
+        private SingletonInfo()                                                                 
+        {
+            Longitude = "";
+            Latitude = "";
+            CurrentURL = "";
+        }
+        public static SingletonInfo GetInstance()
+        {
+            if (_singleton == null)
+            {
+                Interlocked.CompareExchange(ref _singleton, new SingletonInfo(), null);
+            }
+            return _singleton;
+        }
+    }
+}
